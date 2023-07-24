4eca9415322b04b8e21763162ec744f113cc6657
hitesh1193git <136225489+hitesh1193git@users.noreply.github.com>
2023-07-25 01:08:44 +0530

    added dist and src in root directory

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,7 +11,16 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js("src/js/app.js", "dist/js")
+    .js("src/js/ckeditor-classic.js", "dist/js")
+    .js("src/js/ckeditor-inline.js", "dist/js")
+    .js("src/js/ckeditor-balloon.js", "dist/js")
+    .js("src/js/ckeditor-balloon-block.js", "dist/js")
+    .js("src/js/ckeditor-document.js", "dist/js")
+    .css("dist/css/_app.css", "dist/css/app.css")
+    .options({
+        processCssUrls: false,
+    })
+    .copyDirectory("src/json", "dist/json")
+    .copyDirectory("src/fonts", "dist/fonts")
+    .copyDirectory("src/images", "dist/images");
